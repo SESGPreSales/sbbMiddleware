c4190ba778d49085978e9dce9c47a1dfd33f5759
Thomas <tbe@pm.me>
2023-01-24 14:56:49 +0000

    newly created

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 const { stripPrefix } = require('xml2js/lib/processors');
-
+const express = require('express');
+const app = express();
 var parseString = require('xml2js').parseString;
+const fs = require('fs');
+const { sbbApiKey } = require('./environment/secrets');
+//require('./environment/secrets')
 
+const fileName = './public/trains.json';
+//const fileNamePower = './public/power.json';
 
-// from local xmlfile
-
-// function loadXMLDoc() {
-//     var xmlhttp = new XMLHttpRequest();
-//     xmlhttp.onreadystatechange = function() {
-//       if (this.readyState == 4 && this.status == 200) {
-//         //console.log(this.responseXML)
-//         myFunction(this);
-//       }
-//     };
-//     xmlhttp.open("GET", "data.xml", true, );
-//     //xmlhttp.setRequestHeader('Authentication', '57c5dbbbf1fe4d0001000018ebf3689e7a044b00b23e5202e0972bd7')
-//     xmlhttp.send();
-//   };
-
-// const xmlToJSON = require('./xmlToJSON.js')
+let txt = [];
 
 const body = `
 <?xml version="1.0" encoding="UTF-8"?>
@@ -34,7 +25,7 @@ const body = `
                     </LocationRef>
                 </Location>
                 <Params>
-                    <NumberOfResults>20</NumberOfResults>
+                    <NumberOfResults>100</NumberOfResults>
                     <StopEventType>departure</StopEventType>
                     <IncludePreviousCalls>false</IncludePreviousCalls>
                     <IncludeOnwardCalls>false</IncludeOnwardCalls>
@@ -48,33 +39,13 @@ const body = `
 
 const xmlUrl = 'https://api.opentransportdata.swiss/trias2020';
 
-
-//   function loadXMLDoc() {
-//     var xmlhttp = new XMLHttpRequest();
-//     xmlhttp.onreadystatechange = function() {
-//       if (this.readyState == 4 && this.status == 200) {
-//         //console.log(this.responseXML)
-//         myFunction(this);
-//       }
-//     };
-//     xmlhttp.open("PUT", xmlUrl, true );
-//     xmlhttp.setRequestHeader('Access-Control-Allow-Origin', '*')
-//     xmlhttp.setRequestHeader('Authentication', '57c5dbbbf1fe4d0001000018ebf3689e7a044b00b23e5202e0972bd7')
-//     xmlhttp.setRequestHeader('Constent-Type', 'text/XML');
-
-//     xmlhttp.send(body);
-//   };
-let result = {};
-let tree = 'Trias[0].ServiceDelivery[0].DeliveryPayload[0].StopEventResponse[0]'
-
-
 function getData() {
     fetch(xmlUrl, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
         headers: {
             'Content-Type': 'application/XML',
-            'Authorization': '57c5dbbbf1fe4d000100001842c323fa9ff44fbba0b9b925f0c052d1'
+            'Authorization': sbbApiKey
         },
         body // body data type must match "Content-Type" header
     })
@@ -84,31 +55,19 @@ function getData() {
 
                 parseString(text, { tagNameProcessors: [stripPrefix] }, function (err, result) {
 
-                    //console.dir(result);
-
                     let jsonObj = result.Trias.ServiceDelivery[0].DeliveryPayload[0].StopEventResponse[0].StopEventResult;
                     //console.log(jsonObj[0].StopEvent[0].ThisCall[0].CallAtStop[0].PlannedBay[0].Text[0]);
                     myFunction(jsonObj)
 
                 });
 
-                // var jsonObj = xmlToJSON.parseString(text);
-                //console.log(jsonObj)
-                //result = jsonObj.Trias[0].ServiceDelivery[0].DeliveryPayload[0].StopEventResponse[0].StopEventResult;
-                // console.log(result)
-                // myFunction(result)
             })
         })
 }
 
-// setInterval(getData(), 5000);
-getData();
-
-
 function myFunction(xml) {
-    var x, i, txt;
+    var x, i;
     x = xml;
-    txt = [];
 
     // console.log(xml);
 
@@ -116,15 +75,48 @@ function myFunction(xml) {
 
         let track = x[i].StopEvent[0].ThisCall[0].CallAtStop[0].PlannedBay[0].Text[0];
         let train = x[i].StopEvent[0].Service[0].PublishedLineName[0].Text[0];
-        let dir = x[i].StopEvent[0].Service[0].DirectionRef[0];
+        let dir = 'Departure';
         let timePlanned = x[i].StopEvent[0].ThisCall[0].CallAtStop[0].ServiceDeparture[0].TimetabledTime[0];
-        let timeEstimated = x[i].StopEvent[0].ThisCall[0].CallAtStop[0].ServiceDeparture[0].EstimatedTime[0];
+        //let timeEstimated = x[i].StopEvent[0].ThisCall[0].CallAtStop[0].ServiceDeparture[0].EstimatedTime[0];
         let dest = x[i].StopEvent[0].Service[0].DestinationText[0].Text[0];
         let from = x[i].StopEvent[0].Service[0].OriginText[0].Text[0];
 
-        txt.push({ timePlanned, timeEstimated, track, train, dir, dest, from })
+        txt.push({ timePlanned, track, train, dir, dest, from })
     }
 
-    console.log('DataFile (txt)= ', txt);
+    //console.log('DataFile (txt)= ', txt);
 
-}
+    fs.writeFile(fileName, JSON.stringify(txt), function writeJSON(err) {
+        if (err) return console.log(err);
+        //console.log('writing to ' + fileName);
+    });
+};
+
+
+// crete Route to request correct data
+app.get('/api/sbb/track/:track', (req, res) => {
+    console.log('requested track =', req.params.track);
+
+    //get fulldata from file:
+    let contentfull = JSON.parse(fs.readFileSync(fileName))
+
+    let filtered = contentfull.filter(t => t.track === req.params.track);
+    //console.log('filtered ', filtered);
+
+    res.status(200).send(filtered);
+
+    // id = req.params.id;
+    // contentfull[id].occupied = req.params.movement;
+    // console.log('new: ', contentfull);
+
+    // fs.writeFile(fileName, JSON.stringify(contentfull), function writeJSON(err) {
+    // 	if (err) return console.log(err);
+    // 	//  	console.log(JSON.stringify(contentfull));
+    // 	console.log('writing to ' + fileName);
+    // })
+});
+
+setInterval(() => getData(), 30000);
+
+app.listen(5019);
+console.log('listening to port 5019...');
